33fcb3fc6166e2c255b9cdedee28d50d9db33215
Lauranne <lauranne@perfbook.fr>
2024-12-03 20:52:47 +0100

    feat(core): create "add" admin page

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -6,12 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/iNx2bTMIwsRbqHH6ZuiNPQ')]
 class AdminController extends AbstractController
 {
 
-    #[Route('/iNx2bTMIwsRbqHH6ZuiNPQ', name: 'app_admin_list')]
+    #[Route('/', name: 'app_admin_list')]
     public function list(): Response
     {
         return $this->render('Pages/Admin/list.html.twig');
+    }
+
+    #[Route('/add', name: 'app_admin_add')]
+    public function add(): Response
+    {
+        return $this->render('Pages/Admin/add.html.twig');
     }
 }
